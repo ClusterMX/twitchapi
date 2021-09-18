792a80449f9bebbccd76da688679d1464b52bfd7
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-09-18 18:51:54 -0500

    Api twitch

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/token', function (Request $request) {
     $code = $request->get('code');
     $url = env('TWITCH_REDIRECT_URI')."/token?code=".$code;
 
+
+
     return Redirect::to($url);
 
 });
@@ -33,7 +35,7 @@ Route::get('/success', function (Request $request) {
     $code = $request->get('code');
     $url = "https://id.twitch.tv/oauth2/token?client_id=usgzw5f481gonmhmlwkm93gifo0d6t&client_secret=owf2oqgzmajnlyyl6upkjux0ec2u9y&code=".$code."&grant_type=authorization_code&redirect_uri=".env('TWITCH_REDIRECT_URI');
 
-    return $url;
+    // return $url;
 
     return Redirect::to($url);
 });
