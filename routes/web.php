8eab198217df567410129551a4e9399da7950048
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-09-18 19:24:43 -0500

    Api twitch

--- routes/web.php
+++ routes/web.php
@@ -28,18 +28,18 @@ Route::get('/success', function (Request $request) {
     $code = $request->get('code');
     $url = "https://id.twitch.tv/oauth2/token?client_id=".env('TWITCH_CLIENT_ID')."&client_secret=".env('TWITCH_CLIENT_SECRET')."&code=".$code."&grant_type=authorization_code&redirect_uri=".env('TWITCH_REDIRECT_URI');
 
-    return $url;
+    //return $url;
 
-    // return Redirect::to($url);
+    return Redirect::to($url);
 });
 
 
 //Se obtiene el codigo generado
 Route::get('/token', function (Request $request) {
-    $code = $request->get('code');
-    $url = env('TWITCH_REDIRECT_URI')."/token?code=".$code;
+    // $code = $request->get('code');
+    // $url = env('TWITCH_REDIRECT_URI')."/token?code=".$code;
 
-    return Redirect::to($url);
+    // return Redirect::to($url);
 
 });
 
