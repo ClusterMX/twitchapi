44190ac444326b29334ed92718f87d3f2b035d45
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-09-18 19:14:45 -0500

    Api twitch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,40 @@
-<a href="https://id.twitch.tv/oauth2/authorize?client_id={{ env('TWITCH_CLIENT_ID') }}&redirect_uri={{ env('TWITCH_REDIRECT_URI') }}/success/&response_type=code&scope=channel:read:subscriptions">Inicia sesión</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="https://oss.maxcdn.com/semantic-ui/2.2.10/semantic.min.css">
+</head>
+
+<body>
 
 
+    <style>
+        .ui.twitch.button {
+            background-color: #6441A5;
+            color: #FFF;
+            box-shadow: 0 0 0 0 rgba(34, 36, 38, .15) inset
+        }
+
+        .ui.twitch.button:hover {
+            background-color: #3B2064
+        }
+
+    </style>
+
+    <form action="https://id.twitch.tv/oauth2/authorize?client_id={{ env('TWITCH_CLIENT_ID') }}&redirect_uri={{ env('TWITCH_REDIRECT_URI') }}/success/&response_type=code&scope=channel:read:subscriptions" method="post">
+        @csrf
+        <button class="ui twitch button">
+            <i class="twitch icon"></i> Connect with Twitch
+        </button>
+
+    </form>
+
+
+</body>
+
+</html>
