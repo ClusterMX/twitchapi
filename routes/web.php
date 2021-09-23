15266c1b45664c9e400bd6de8d584c4fdad48d8e
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-09-22 19:53:39 -0500

    appi twitch

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,27 @@ Route::get('/success', function (Request $request) {
     $code_twitch = $request->get('code');
     $url = "https://id.twitch.tv/oauth2/token?client_id=".env('TWITCH_CLIENT_ID')."&client_secret=".env('TWITCH_CLIENT_SECRET')."&code=".$code_twitch."&grant_type=authorization_code&redirect_uri=".env('TWITCH_REDIRECT_URI')."/token";
 
+    // $endpoint = "https://id.twitch.tv/oauth2/token";
+
+    // $apiParam = array(
+    //     'endpoint' => $endpoint,
+    //     'type' => 'POST',
+    //     'url_params' => array(
+    //         'client_id' => env('TWITCH_CLIENT_ID'),
+    //         'client_secret' => env('TWITCH_CLIENT_SECRET'),
+    //         'code' => $code_twitch,
+    //         'grant_type' => 'authorization_code',
+    //         'redirect_uri' => env('TWITCH_REDIRECT_URI')."/token"
+    //     )
+    //  );
+
+    //  return $this->makeAppiCall($apiParam);
+
+    $client = new GuzzleHttp\Client();
+    $res = $client->get($url);
+
+    return $res->getBody();
+
     // return $code_twitch;
 
     // return Redirect::to($url);
