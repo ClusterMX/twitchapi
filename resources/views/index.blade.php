3c3bed80f7cfea69904a788614bc22b033afe037
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-09-22 20:02:03 -0500

    appi twitch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,35 +8,48 @@
     <title>Login</title>
 
     <link rel="stylesheet" href="https://oss.maxcdn.com/semantic-ui/2.2.10/semantic.min.css">
+
+    <link href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" rel="stylesheet">
 </head>
 
 <body>
 
 
     <style>
-        .ui.twitch.button {
-            background-color: #6441A5;
-            color: #FFF;
-            box-shadow: 0 0 0 0 rgba(34, 36, 38, .15) inset
-        }
+        .login__wrapper-B .login__inner .button {
+    text-transform: none;
+}
 
-        .ui.twitch.button:hover {
-            background-color: #3B2064
-        }
+.night .button--twitch {
+    background-color: #9146ff;
+    color: #fff;
+}
 
     </style>
 
-    <form action="" method="post">
-        {{-- @csrf --}}
-        <button class="ui twitch button">
-            <i class="twitch icon"></i> Connect with Twitch
-        </button>
-
-    </form>
-
-    <a href="https://id.twitch.tv/oauth2/authorize?client_id={{ env('TWITCH_CLIENT_ID') }}&redirect_uri={{ env('TWITCH_REDIRECT_URI') }}/success&response_type=code&scope=channel:read:subscriptions" class="ui twitch button"> <i class="twitch icon"></i> Connect with twitch</a>
 
 
+{{--
+    <a href="" class="ui twitch button"> <i class="twitch icon"></i> Connect with twitch</a>--}}
+
+    <button id="connect-with-twitch" class="button login-button button--twitch" data-platform="twitch">
+        <i class="fab fa-twitch"></i>
+        <span>Continue with Twitch</span>
+    </button>
+
+
+
+
+    <script>
+        $('#connect-with-twitch').click(function(e) {
+            e.preventDefault();
+
+            ga('send', 'event', 'firstLogin', 'click', 'twitch', '1');
+
+            window.location =
+                "https://id.twitch.tv/oauth2/authorize?client_id="+{{ env('TWITCH_CLIENT_ID') }}+"&redirect_uri="+{{ env('TWITCH_REDIRECT_URI') }}+"/success&response_type=code&scope=channel:read:subscriptions";
+        });
+    </script>
 </body>
 
 </html>
