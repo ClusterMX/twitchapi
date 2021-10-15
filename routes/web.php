32cae3309d5ed8efab4a166c989a893ab3dcd519
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-10-15 18:36:12 -0500

    Api Twitch

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,6 @@ Route::get('/', function () {
 //Se utiliza para crear el token y poder acceder
 Route::get('/success', function (Request $request) {
 
-    //Genero el token
-
-
 
     //Se obtiene el codigo de autorización
     $code_twitch = $request->get('code');
@@ -37,17 +34,7 @@ Route::get('/success', function (Request $request) {
     $res = $client->post($url);
     $datos = json_decode($res->getBody()->getContents());
 
-    $token = $datos->access_token; //Bearer
-
-    // Creo el request del oauth2 validate - https://id.twitch.tv/oauth2/validate y obtengo user_id
-    $user = "https://id.twitch.tv/oauth2/validate";
-    $u = $client->get($user, [
-        'headers' => [
-            'Authorization' => 'Bearer ' . $token,
-        ],
-    ]);
-
-    return $u;
+    $token = $datos->access_token;
 
 
     //Se obtienen los datos de los subs
@@ -75,7 +62,17 @@ Route::get('/datos', function (Request $request) {
 
     $token = $request->get('token');
 
+
+
+
     $client = new Client();
+
+
+
+
+
+
+
 
     return $datos;
 
