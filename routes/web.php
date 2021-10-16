b3f09b7dd0520f5413f98770897305af5f6e255f
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-10-16 00:34:28 -0500

    Api Twitch

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ Route::get('/success', function (Request $request) {
 
 
 
-    return redirect()->route('board')->with( ['datos_subs' => $datos_subs] );
+    return redirect()->route('board')->with('datos_subs', $datos_subs);
 
 });
 
