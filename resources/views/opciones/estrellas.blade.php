facb60299b572018320f40603e82f226bbaf7386
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-16 21:42:02 -0600

    Api Twitch

--- resources/views/opciones/estrellas.blade.php
+++ resources/views/opciones/estrellas.blade.php
@@ -46,7 +46,9 @@
     @foreach ($datos_subs as $ds)
 
         {{ $i = $loop->index +1 }}
-        <div class="star{{ $i }} " style="position: absolute; display:none; animation: glow {{ mt_rand(1, 3) }}s linear alternate infinite;"><i style="font-size: 15px; color:gold;" class="fas fa-star"></i></div>
+        <div class="star{{ $i }} " style="position: absolute; display:none; animation: glow {{ mt_rand(1, 3) }}s linear alternate infinite;">
+            <i style="font-size: 15px; color:gold;" class="fas fa-star"></i><br><span style="color:white;">{{ $ds->user_name }}</span>
+        </div>
 
 
 
