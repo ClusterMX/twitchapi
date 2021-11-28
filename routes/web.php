1e44fe01294cec609607410de9d70dc7cf742d55
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-28 02:28:42 -0600

    Twitch api

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/auth/twitch/redirect', function () {
 
 Route::get('/auth/twitch/callback', function () {
     $user = Socialite::driver('twitch')->user();
-   dd($user);
+    return $user;
 
     // $user->token
 });
