6babe474e10aeb40f64d753b94def465e0588603
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-29 22:53:17 -0600

    Api Twitch

--- app/Http/Controllers/EventSubController.php
+++ app/Http/Controllers/EventSubController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use romanzipp\Twitch\Http\Controllers\EventSubController as BaseController;
 use Symfony\Component\HttpFoundation\Response;
 
-use Log;
 
 class EventSubController extends BaseController
 {
@@ -15,10 +14,7 @@ class EventSubController extends BaseController
     {
         return $this->successMethod(); // handle the channel follow notification...
     }
-    public function handleChannelChannelPointsCustomRewardRedemptionAddNotification(array $payload): Response
-    {
-        return $this->successMethod(); // handle the channel follow notification...
-    }
+
     protected function handleNotification(array $payload): Response
     {
         return $this->successMethod(); // handle all other incoming notifications...
@@ -28,6 +24,4 @@ class EventSubController extends BaseController
     {
         return $this->successMethod(); // handle the subscription revocation...
     }
-
-
 }
--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::post(
-    '/twitch/eventsub/webhook',
+    'twitch/eventsub/webhook',
     [EventSubController::class, 'handleWebhook']
 );
 
