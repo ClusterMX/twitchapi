43844daf3740821651212acd60ad407ce7c8cd3d
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-29 22:57:59 -0600

    Api Twitch

--- routes/web.php
+++ routes/web.php
@@ -66,18 +66,18 @@ Route::get('/auth/twitch/callback', function () {
 
     $twitch = new Twitch;
 
-    // $result = $twitch->subscribeEventSub([], [
-    //     'type' => 'channel.channel_points_custom_reward_redemption.add', //EventSubType::CHANNEL_CHANNEL_POINTS_CUSTOM_REWARD_REDEMPTION_REMOVE,
-    //     'version' => '1',
-    //     'condition' => [
-    //         'broadcaster_user_id' => $user->id,
-    //     ],
-    //     'transport' => [
-    //         'method' => 'webhook',
-    //         'callback' => 'https://twitchapi.clustermx.com/api/twitch/eventsub/webhook',
-    //         // 'secret' => 'chenchosecret',
-    //     ]
-    // ]);
+    $result = $twitch->subscribeEventSub([], [
+        'type' => 'channel.channel_points_custom_reward_redemption.add', //EventSubType::CHANNEL_CHANNEL_POINTS_CUSTOM_REWARD_REDEMPTION_REMOVE,
+        'version' => '1',
+        'condition' => [
+            'broadcaster_user_id' => $user->id,
+        ],
+        'transport' => [
+            'method' => 'webhook',
+            'callback' => 'https://twitchapi.clustermx.com/api/twitch/eventsub/webhook',
+            // 'secret' => 'chenchosecret',
+        ]
+    ]);
 
 
 
@@ -88,9 +88,9 @@ Route::get('/auth/twitch/callback', function () {
     //     echo $item.'<br>';
     // }
 
-    $result = $twitch->unsubscribeEventSub([
-        'id' => '11c32de9-4c24-4dce-8e6e-e3fdaa876ef4'
-    ]);
+    // $result = $twitch->unsubscribeEventSub([
+    //     'id' => '11c32de9-4c24-4dce-8e6e-e3fdaa876ef4'
+    // ]);
 
 
     return $result->data();
