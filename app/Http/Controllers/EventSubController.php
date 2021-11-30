b8dc86ad125c900e88f884f2c5129f1174067c84
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-29 22:38:41 -0600

    Api Twitch

--- app/Http/Controllers/EventSubController.php
+++ app/Http/Controllers/EventSubController.php
@@ -8,46 +8,10 @@ use romanzipp\Twitch\Http\Controllers\EventSubController as BaseController;
 use Symfony\Component\HttpFoundation\Response;
 
 use Log;
-use Carbon\Carbon;
 
 class EventSubController extends BaseController
 {
 
 
-    public function handleWebhookRedeem(Request $request): Response {
 
-
-
-        $payload = json_decode($request->getContent(), true);
-
-        $messageType = $request->header('twitch-eventsub-message-type');
-        $messageId = $request->header('twitch-eventsub-message-id');
-        $retries = (int) $request->header('twitch-eventsub-message-retry');
-        $timestamp = Carbon::createFromTimestampUTC(
-                        EventSubSignature::getTimestamp($request->header('twitch-eventsub-message-timestamp')));
-
-        if ('notification' === $messageType) {
-            $messageType = sprintf('%s.notification', $payload['subscription']['type']);
-        }
-
-        $method = 'handle' . Str::studly(str_replace('.', '_', $messageType));
-
-        EventSubReceived::dispatch($payload, $messageId, $retries, $timestamp);
-
-        if (method_exists($this, $method)) {
-            $response = $this->{$method}($payload);
-
-            EventSubHandled::dispatch($payload, $messageId, $retries, $timestamp);
-
-            Log::info($response);
-
-            return $response;
-
-        }
-
-        Log::info($this->missingMethod());
-
-        return $this->missingMethod();
-
-    }
 }
