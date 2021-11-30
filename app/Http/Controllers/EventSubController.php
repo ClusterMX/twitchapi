e021369d1ec5f4886e8c836acff9e282d740bb04
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-29 23:05:14 -0600

    Api Twitch

--- app/Http/Controllers/EventSubController.php
+++ app/Http/Controllers/EventSubController.php
@@ -7,12 +7,28 @@ use Illuminate\Http\Request;
 use romanzipp\Twitch\Http\Controllers\EventSubController as BaseController;
 use Symfony\Component\HttpFoundation\Response;
 
+use romanzipp\Twitch\Events\EventSubHandled;
+use romanzipp\Twitch\Events\EventSubReceived;
+use romanzipp\Twitch\Http\Middleware\VerifyEventSubSignature;
+use romanzipp\Twitch\Objects\EventSubSignature;
+
 
 class EventSubController extends BaseController
 {
     public function handleChannelFollowNotification(array $payload): Response
     {
         return $this->successMethod(); // handle the channel follow notification...
+    }
+
+
+    protected function handleNotification(array $payload): Response
+    {
+        return $this->successMethod();
+    }
+
+    protected function handleRevocation(array $payload): Response
+    {
+        return $this->successMethod(); // handle the subscription revocation...
     }
 
 
