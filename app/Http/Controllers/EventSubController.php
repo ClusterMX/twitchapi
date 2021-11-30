8c678641b195f578ed55272d0d01ab1e978a828d
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-11-29 22:41:09 -0600

    Api Twitch

--- app/Http/Controllers/EventSubController.php
+++ app/Http/Controllers/EventSubController.php
@@ -11,7 +11,23 @@ use Log;
 
 class EventSubController extends BaseController
 {
+    public function handleChannelFollowNotification(array $payload): Response
+    {
+        return $this->successMethod(); // handle the channel follow notification...
+    }
+    public function handleChannelChannelPointsCustomRewardRedemptionAddNotification(array $payload): Response
+    {
+        return $this->successMethod(); // handle the channel follow notification...
+    }
+    protected function handleNotification(array $payload): Response
+    {
+        return $this->successMethod(); // handle all other incoming notifications...
+    }
 
+    protected function handleRevocation(array $payload): Response
+    {
+        return $this->successMethod(); // handle the subscription revocation...
+    }
 
 
 }
