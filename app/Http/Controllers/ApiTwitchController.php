7e83d2de4bcd0f36153100660d545c1655c991d1
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-12-18 20:28:58 -0600

    Api Twitch

--- app/Http/Controllers/ApiTwitchController.php
+++ app/Http/Controllers/ApiTwitchController.php
@@ -164,7 +164,7 @@ class ApiTwitchController extends Controller
 
 
         // $result = $twitch->subscribeEventSub([],$payload);
-        $resultad = $twitch->getEventSubs(['status' => 'enabled']);
+        $resultad = $twitch->getEventSubs(['status' => 'webhook_callback_verification_failed']);
 
         // $result = $twitch->withToken($access_token)->subscribeEventSub([],$payload);
         // $resultad = $twitch->withToken($access_token)->getEventSubs(['status' => 'webhook_callback_verification_pending ']);
