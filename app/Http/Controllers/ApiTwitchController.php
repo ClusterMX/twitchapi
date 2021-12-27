8a60552b494672eb4e9c62a950eaffb227ba3d41
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-12-26 23:53:26 -0600

    api twitch

--- app/Http/Controllers/ApiTwitchController.php
+++ app/Http/Controllers/ApiTwitchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 use GuzzleHttp\Client;
 use Laravel\Socialite\Facades\Socialite;
