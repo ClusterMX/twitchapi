81e7f96aad45864163eb546d6b3dc4db21fcb26d
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-12-26 23:42:46 -0600

    api twitch

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'twitch_id',
+        'twitch_token',
+        'twitch_refresh',
+        'avatar',
     ];
 
     /**
