cdff3e28d29befa989dd9191101247cffa4a38eb
Alfredo Galvan <alfredogalvan.91@gmail.com>
2022-01-04 05:26:04 -0600

    Api twitch

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         $twitch = new Twitch;
         $result = $twitch->getUsersFollows(['to_id' => Auth::user()->twitch_id]);
-        return $result;
+        return json_decode($result);
         return view('dashboard.main');
     })->name('dashboard');
 
