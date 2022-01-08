679d907449ccae043f1d9cf2dfcfff820c832825
Alfredo Galvan <alfredogalvan.91@gmail.com>
2022-01-08 16:52:47 -0600

    Api Twitch

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         $twitch = new Twitch;
 
-        $twitch = $twitch->getUsers(['login' => Auth::user()->name]);
 
         $result = $twitch->getOAuthToken(null, GrantType::CLIENT_CREDENTIALS, ['user:read:email', 'user:edit:follows', 'channel:read:subscriptions']);
 
