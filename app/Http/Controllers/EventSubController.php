802848877caa3e0cce8ad7e93c5c837685a046d9
Alfredo Galvan <alfredogalvan.91@gmail.com>
2022-01-08 19:21:28 -0600

    Api Twitch

--- app/Http/Controllers/EventSubController.php
+++ app/Http/Controllers/EventSubController.php
@@ -129,8 +129,7 @@ class EventSubController extends Controller
         Log::info('handleChannelFollowNotification');
         //convertimos el array en un objeto
         $result = json_encode($payload);
-        Log::info($result->subscription);
-        Log::info($result->event);
+        Log::info($array->subscription);
 
         return $this->successMethod(); // handle the channel follow notification...
     }
