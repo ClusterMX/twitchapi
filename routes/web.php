0d80c3810b28dc0e9f87b1a5fd89e0cc06b6b42d
Alfredo Galvan <alfredogalvan.91@gmail.com>
2022-01-08 18:16:11 -0600

    Api Twitch

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,7 @@ Route::middleware(['auth'])->group(function () {
         $search  = array('%{width}', '%{height}');
         $replace = array('320', '180');
 
-        dd($subs[0]->total);
+        dd($subs->total);
         return view('dashboard.main', compact('followers', 'viewcount', 'videos', 'search', 'replace'));
     })->name('dashboard');
 
