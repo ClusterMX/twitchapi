a6cd24f1be5fa2750fb1bee402f9bb3d1190d060
Alfredo Galvan <alfredogalvan.91@gmail.com>
2022-01-09 01:56:37 -0600

    Api Twitch

--- app/Http/Controllers/EventSubController.php
+++ app/Http/Controllers/EventSubController.php
@@ -1,129 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Routing\Controller;
-use Illuminate\Support\Str;
-use romanzipp\Twitch\Events\EventSubHandled;
-use romanzipp\Twitch\Events\EventSubReceived;
-use romanzipp\Twitch\Http\Middleware\VerifyEventSubSignature;
-use romanzipp\Twitch\Objects\EventSubSignature;
-use Log;
+use romanzipp\Twitch\Http\Controllers\EventSubController as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
-class EventSubController extends Controller
+class EventSubController extends BaseController
 {
-    public function __construct()
+    public function handleChannelFollowNotification(array $payload): Response
     {
-        if (config('twitch-api.eventsub.secret')) {
-            $this->middleware(VerifyEventSubSignature::class);
-        }
+        return $this->successMethod(); // handle the channel follow notification...
     }
 
-    /**
-     * Handle a Twitch webhook call.
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function handleWebhook(Request $request): Response
-    {
-        $payload = json_decode($request->getContent(), true);
-
-        $messageType = $request->header('twitch-eventsub-message-type');
-        $messageId = $request->header('twitch-eventsub-message-id');
-        $retries = (int) $request->header('twitch-eventsub-message-retry');
-        $timestamp = Carbon::createFromTimestampUTC(
-                        EventSubSignature::getTimestamp($request->header('twitch-eventsub-message-timestamp')));
-
-        if ('notification' === $messageType) {
-            $messageType = sprintf('%s.notification', $payload['subscription']['type']);
-        }
-
-        $method = 'handle' . Str::studly(str_replace('.', '_', $messageType));
-
-        Log::info('method de handleWebhook');
-        Log::info($method);
-
-        EventSubReceived::dispatch($payload, $messageId, $retries, $timestamp);
-
-        if (method_exists($this, $method)) {
-            $response = $this->{$method}($payload);
-
-            EventSubHandled::dispatch($payload, $messageId, $retries, $timestamp);
-
-            // Log::info('response de handleWebhook');
-            // Log::info($response);
-
-            return $response;
-        }
-
-        return $this->missingMethod();
-    }
-
-    /**
-     * Handle a EventSub callback verification call.
-     *
-     * @param array<string, mixed> $payload
-     *
-     * @return Response
-     */
-    protected function handleWebhookCallbackVerification(array $payload): Response
-    {
-        app('debugbar')->disable();
-        return new Response($payload['challenge'], 200);
-    }
-
-    /**
-     * Handle a EventSub notification call.
-     *
-     * @param array<string, mixed> $payload
-     *
-     * @return Response
-     */
     protected function handleNotification(array $payload): Response
     {
-        return $this->successMethod();
+        return $this->successMethod(); // handle all other incoming notifications...
     }
 
-    /**
-     * Handle a EventSub revocation call.
-     *
-     * @param array<string, mixed> $payload
-     *
-     * @return Response
-     */
     protected function handleRevocation(array $payload): Response
     {
-        return $this->successMethod();
+        return $this->successMethod(); // handle the subscription revocation...
     }
 
-    /**
-     * Handle successful calls on the controller.
-     *
-     * @param array<string, mixed> $parameters
-     *
-     * @return Response
-     */
-    protected function successMethod(array $parameters = []): Response
-    {
-        return new Response('Webhook Handled', 200);
-    }
-
-    /**
-     * Handle calls to missing methods on the controller.
-     *
-     * @param array<string, mixed> $parameters
-     *
-     * @return Response
-     */
-    protected function missingMethod($parameters = []): Response
-    {
-        return new Response();
-    }
 
     public function handleChannelFollowNotification(array $payload): Response
     {
